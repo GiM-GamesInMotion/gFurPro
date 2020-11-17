038c9b537f4aba026c3ada5e14fe8a5850705bd0
JurajBleho <35736982+JurajBleho@users.noreply.github.com>
2020-11-17 19:45:31 +0100

    fixed splines threshold

--- Intermediate/Build/Win64/UE4/Inc/GFur/FurSplines.generated.h
+++ Intermediate/Build/Win64/UE4/Inc/GFur/FurSplines.generated.h
@@ -36,7 +36,7 @@ public: \
 
 #define HostProject_Plugins_GFur_Source_GFur_Public_FurSplines_h_10_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
-	NO_API UFurSplines(const FObjectInitializer& ObjectInitializer); \
+	NO_API UFurSplines(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UFurSplines) \
 	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFurSplines); \
 DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFurSplines); \
@@ -48,6 +48,8 @@ public:
 
 
 #define HostProject_Plugins_GFur_Source_GFur_Public_FurSplines_h_10_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UFurSplines(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	NO_API UFurSplines(UFurSplines&&); \
@@ -55,7 +57,7 @@ private: \
 public: \
 	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UFurSplines); \
 DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UFurSplines); \
-	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UFurSplines)
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UFurSplines)
 
 
 #define HostProject_Plugins_GFur_Source_GFur_Public_FurSplines_h_10_PRIVATE_PROPERTY_OFFSET
